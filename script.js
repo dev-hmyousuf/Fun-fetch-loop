f63bc595b2c0576e8c58ad59cc817b37160700f1
Developer Yousuf <hmyousuf2010@gmail.com>
2024-10-19 21:30:48 +0600

    DF

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ async function startWork() {
     const response = await fetch('https://fun-fetch-api-test.vercel.app/json.js');
     const data = await response.json();
 
-    const ammount = await parseInt(data.loopAmount);
+    const amount = parseInt(data.loopAmount);
     const demoElement = document.getElementById("demo");
-    demoElement.innerHTML = "";
+    demoElement.innerHTML = ""; 
 
     let i = 1;
-    while(i <= ammount) {
-        demoElement.innerHTML += `<span>${i}</span> ,`
-        i++ ;
+    while (i <= amount) {
+        demoElement.innerHTML += `<span>${i}</span><br>`; 
+        i++;
     }
 }
